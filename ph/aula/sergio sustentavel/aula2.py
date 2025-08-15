fa636d9fbbccc55cb27edf930abbee48972d5450
raphael <r4faelsouza09@gmail.com>
2025-08-15 11:34:17 -0300

    aula sergio 2

---
+++ ph/aula/sergio sustentavel/aula2.py
@@ -0,0 +1,20 @@
+postular = int("afimrar com insitencia")
+na parte do beta 
+um dos betas do nucleo pai(quarks) vira proton eletron, neutrino
+
+
+gama = uma liberacao de energia q n altera nada na composicao
+ele sai do estado fundamental para o excitante depois retornando
+
+
+na parte do alpha 
+ele libera helio quando dissolve do nucleo pai 
+uranio = torio + alpha
+
+energia nuclear pode c adquirir por dois processos 
+fissao nuclear 
+fusao nuclear
+
+
+
+isotopos instaveis sao quando a soma das duas massas sao diferentes da massa original 
